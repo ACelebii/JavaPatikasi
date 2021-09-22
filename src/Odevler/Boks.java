412ebfa3895a79191a5b49a558829748318324a0
ACelebii <ahmet.celebii@hotmail.com>
2021-09-22 16:38:13 +0300

    OgrenciNotSistemi, Course, Student, Teacher

---
+++ src/Odevler/Boks.java
@@ -0,0 +1,10 @@
+package Odevler;
+
+public class Boks {
+    public static void main(String[] args) {
+        Fighter marc = new Fighter("Marc", 15, 100, 90, 35);
+        Fighter alex = new Fighter("Alex", 10, 95, 100, 30);
+        Ring r = new Ring(marc, alex, 90, 100);
+        r.run();
+    }
+}
